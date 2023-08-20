32ef0af20d9068d7caff7cb70d9a9b5f343c393c
luca-cardi <giovanniluca.cardillo@gmail.com>
2023-08-20 16:50:35 +0100

    fix bug and add navigation

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
   Text,
@@ -8,136 +7,33 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { API_URL, API_TOKEN } from "@env";
 import { Icon } from "@rneui/base";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Search } from "./component/Search";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [searchText, setSearchText] = useState<string>("");
-
-  const [results, setResults] = useState<any>([]);
-  const getData = async (search: string) => {
-    setSearchText(search);
-    if (search !== "") {
-      try {
-        const response = await axios.get(
-          `${API_URL}/search?q=${search}&type=album%2Cartist%2Ctrack`,
-          { headers: { Authorization: `Bearer ${API_TOKEN}` } }
-        );
-        const { artists } = response.data;
-        setResults(artists.items);
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
-      setResults([]);
-    }
-  };
-  console.log(results[0]);
-
-  const handleClear = () => {
-    setSearchText("");
-    setResults([]);
-  };
-
   return (
-    <View style={styles.container}>
-      <View style={styles.searchContainer}>
-        <Icon name="search" color="white" style={styles.icon} />
-        <TextInput
-          style={styles.searchInput}
-          onChangeText={(txt) => getData(txt)}
-          value={searchText}
-          placeholder="Search.."
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Search"
+          component={Search}
+          options={{
+            headerStyle: {
+              backgroundColor: "#2D2D30",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
         />
-        {searchText !== "" && (
-          <TouchableOpacity onPress={handleClear}>
-            <Icon
-              name="remove"
-              color="white"
-              type="font-awesome"
-              style={styles.icon}
-            />
-          </TouchableOpacity>
-        )}
-      </View>
-      <ScrollView style={styles.resultsContainer}>
-        {results.length > 0 &&
-          results.map((result: any, i: number) => (
-            <View style={styles.resultContainer} key={i}>
-              <Image
-                style={styles.images}
-                source={{
-                  uri: result.images[0]?.url,
-                }}
-              />
-              <View>
-                <Text style={styles.name}>{result.name}</Text>
-                <Text style={styles.type}>{result.type}</Text>
-              </View>
-            </View>
-          ))}
-      </ScrollView>
-    </View>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#2D2D30",
-    alignItems: "center",
-    paddingTop: 70,
-  },
-  searchContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 8,
-    paddingHorizontal: 8,
-    width: "90%",
-  },
-  searchInput: {
-    flex: 1,
-    paddingLeft: 8,
-    paddingRight: 30, // To make space for the clear icon
-    color: "white",
-    borderColor: "white",
-    height: 35,
-  },
-  icon: {
-    width: 24,
-    height: 24,
-    marginHorizontal: 4,
-  },
-  images: {
-    width: 60,
-    height: 60,
-    borderRadius: 50,
-  },
-  resultsContainer: {
-    width: '100%',
-    marginTop: 15,
-  },
-  resultContainer: {
-    marginTop: 20,
-    width: "70%",
-    marginLeft: 20,
-    display: "flex",
-    flexDirection: "row",
-    gap: 10,
-    alignItems: "center",
-  },
-  infoContainer: {},
-  name: {
-    fontWeight: "bold",
-    color: "white",
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  type: {
-    color: "white",
-  },
-});
---
+++ component/Search.tsx
@@ -0,0 +1,143 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { API_URL, API_TOKEN } from "@env";
+import { Icon } from "@rneui/base";
+
+export const Search = () => {
+    const [searchText, setSearchText] = useState<string>("");
+    //console.log(API_TOKEN)
+    const [results, setResults] = useState<any>([]);
+
+    const getData = async (search: string) => {
+      const searchFormatted = search.replaceAll(" ", "+");
+      console.log(search);
+      setSearchText(search);
+      if (search !== "") {
+        try {
+          const response = await axios.get(
+            `${API_URL}/search?q=${searchFormatted}&type=album%2Cartist%2Ctrack`,
+            { headers: { Authorization: `Bearer ${API_TOKEN}` } }
+          );
+          const { artists } = response.data;
+          setResults(artists.items);
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        setResults([]);
+      }
+    };
+    // console.log(results[0]);
+
+    const handleClear = () => {
+      setSearchText("");
+      setResults([]);
+    };
+  return (
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <Icon name="search" color="white" style={styles.icon} />
+        <TextInput
+          style={styles.searchInput}
+          onChangeText={(txt) => getData(txt)}
+          value={searchText}
+          placeholder="Search.."
+        />
+        {searchText !== "" && (
+          <TouchableOpacity onPress={handleClear}>
+            <Icon
+              name="remove"
+              color="white"
+              type="font-awesome"
+              size={15}
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+      <ScrollView style={styles.resultsContainer}>
+        {results.length > 0 &&
+          results.map((result: any, i: number) => (
+            <View style={styles.resultContainer} key={i}>
+              <Image
+                style={styles.images}
+                source={{
+                  uri: result.images[0]?.url,
+                }}
+              />
+              <View>
+                <Text style={styles.name}>{result.name}</Text>
+                <Text style={styles.type}>{result.type}</Text>
+              </View>
+            </View>
+          ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#2D2D30",
+    alignItems: "center",
+    paddingTop: 10,
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 0.5,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    width: "90%",
+  },
+  searchInput: {
+    flex: 1,
+    paddingLeft: 8,
+    paddingRight: 30, // To make space for the clear icon
+    color: "white",
+    borderColor: "white",
+    height: 35,
+  },
+  icon: {
+
+  },
+  images: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+  },
+  resultsContainer: {
+    width: "100%",
+    marginTop: 15,
+  },
+  resultContainer: {
+    marginTop: 20,
+    width: "70%",
+    marginLeft: 20,
+    display: "flex",
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+  },
+  infoContainer: {},
+  name: {
+    fontWeight: "bold",
+    color: "white",
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  type: {
+    color: "white",
+  },
+});
